f24b0e8798f20180616b91f2427f514579c70990
t0nqwer <oottoonnoo@gmail.com>
2024-07-08 19:30:36 +0700

    refactor: Remove commented out code in test route

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 
 export const maxDuration = 20;
-export const runtime = "edge";
+// export const runtime = "edge";
 export async function GET(request: Request) {
   console.info("adkmaks");
 
